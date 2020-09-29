6ce7cd8e44e97388b918837f0fa9178b1e342007
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2020-09-29 20:56:38 +0530

    Started the UI

---
+++ lib/data/data.dart
@@ -0,0 +1,57 @@
+import 'package:walli/model/categoriesModel.dart';
+
+List<CategoriesModel> getCategories(){
+
+  List<CategoriesModel> categories = new List();
+  CategoriesModel categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/black-road-bike-leaning-on-red-blue-and-white-wing-graffiti-wall-545008/";
+  categoryModel.categoriesName="Street Art";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/alberta-amazing-attraction-banff-417074/";
+  categoryModel.categoriesName="Nature";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/abstract-art-blur-bright-373543/";
+  categoryModel.categoriesName="Technology";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/flamingo-spreading-its-wings-158471/";
+  categoryModel.categoriesName="Wild Life";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/parked-white-coupe-during-night-3354648/";
+  categoryModel.categoriesName="Cars";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/black-road-bike-leaning-on-red-blue-and-white-wing-graffiti-wall-545008/";
+  categoryModel.categoriesName="Street Art";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/black-road-bike-leaning-on-red-blue-and-white-wing-graffiti-wall-545008/";
+  categoryModel.categoriesName="Street Art";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/black-road-bike-leaning-on-red-blue-and-white-wing-graffiti-wall-545008/";
+  categoryModel.categoriesName="Street Art";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/black-road-bike-leaning-on-red-blue-and-white-wing-graffiti-wall-545008/";
+  categoryModel.categoriesName="Street Art";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+
+  categoryModel.categoryImage="https://www.pexels.com/photo/black-road-bike-leaning-on-red-blue-and-white-wing-graffiti-wall-545008/";
+  categoryModel.categoriesName="Street Art";
+  categories.add(categoryModel);
+  categoryModel = new CategoriesModel();
+}
