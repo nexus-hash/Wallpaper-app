bc96e8999b1dddedc4efb47d83474a4205eb83e1
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2020-09-30 09:08:24 +0530

    Added Image View

--- lib/views/imageView.dart
+++ lib/views/imageView.dart
@@ -18,16 +18,13 @@ class _ImageViewState extends State<ImageView> {
         child: Container(
           child: Stack(
             children: <Widget>[
-
-                Container(
+              Container(
                   height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    child: Image.network(
-                  widget.imageUrl,
-                  fit: BoxFit.cover,
-                    )
-                ),
-
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.network(
+                    widget.imageUrl,
+                    fit: BoxFit.cover,
+                  )),
               Container(
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
@@ -35,38 +32,67 @@ class _ImageViewState extends State<ImageView> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width/2,
-                      height: MediaQuery.of(context).size.height*.08,
-                      padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.white60,width: 1),
-                          borderRadius: BorderRadius.circular(30),
-                          gradient: LinearGradient(colors: [
-                            Color(0x36ffffff),
-                            Color(0x0fffffff),
-                          ])
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Text("Set as Wallpaper",
-                           style: TextStyle(
-                             fontSize: 16
-                           ),
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.height * .08,
+                          decoration: BoxDecoration(
+                              border:
+                              Border.all(color: Colors.white60, width: 1),
+                              borderRadius: BorderRadius.circular(30),
+                            color: Color(0xff1c1b1b)
+                              ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.height * .08,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 8.0),
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.white60, width: 1),
+                              borderRadius: BorderRadius.circular(30),
+                              gradient: LinearGradient(colors: [
+                                Color(0x36ffffff),
+                                Color(0x0fffffff),
+                              ])),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Set as Wallpaper",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Image will be saved in gallery",
+                                style: TextStyle(
+                                    fontSize: 10,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ],
                           ),
-                          SizedBox(height: 5,),
-                          Text("Image will be saved in gallery",style: TextStyle(
-                              fontSize: 10
-                          ),),
-                        ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .035,
+                    ),
+                    Text(
+                      "Cancel",
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
                     ),
-                    SizedBox(height: MediaQuery.of(context).size.height*.035,),
-                    Text("Cancel",style: TextStyle(
-                      color: Colors.white,
-
-                    ),),
-                    SizedBox(height: MediaQuery.of(context).size.height*.06,)
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .06,
+                    )
                   ],
                 ),
               )
